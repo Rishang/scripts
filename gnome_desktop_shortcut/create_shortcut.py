99fc0548f7a66a2852b410f572f6f2e52f257f61
Rishang <rishangbhavsarcs@gmail.com>
2021-07-19 10:15:00 +0530

    gtk desktop icon

---
+++ gnome_desktop_shortcut/create_shortcut.py
@@ -0,0 +1,23 @@
+
+
+app_category="Application"
+
+print("Enter Application Name:",end='')
+app_name=input()
+print("Enter Application Path:",end='')
+app_exec=input()
+print("Enter Application Icon:",end='')
+app_icon=input()
+
+desktop_s=f"""
+#!/usr/bin/env {app_exec}
+[Desktop Entry]
+Type=Application
+Name={app_name}
+Exec="{app_exec}" %U
+Icon={app_icon}
+Categories=Application;
+"""
+
+with open(f"~/.local/share/applications/{app_name.replace(' ','_').lower()}.desktop",'w') as s:
+    s.write(desktop_s)
